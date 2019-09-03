7b66d5781e9ba559b459b84cbbda493294850e71
William Grigor <bjgrigor@gmail.com>
2019-09-03 13:38:26 -0500

    Testing jenkins ci

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import unittest
 
 class TestNetwork(unittest.TestCase):
-    def always_true(self):
+    def test_always_true(self):
         self.assertTrue(True)
+
+    def test_always_fail(self):
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
