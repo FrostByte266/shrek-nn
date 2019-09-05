be7b8c8f83bea89b82cacab6c288fb42dd35d931
William Grigor <bjgrigor@gmail.com>
2019-09-05 14:50:41 -0500

    Homophone improvements

--- src/homophone.py
+++ src/homophone.py
@@ -1,17 +1,23 @@
-import fuzzy
+from fuzzy import nysiis
+from fuzzywuzzy import fuzz
 import csv
 import numpy as np
-
-def generate_dmeta_pair(row):
-    dmeta = fuzzy.DMetaphone()
-    return dmeta(row[0]), dmeta(row[1])
 
 def get_all_pairs(csv_file):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
-            yield generate_dmeta_pair(row)
+            yield nysiis(row[0]), nysiis(row[1])
 
-def check_equal(meta_tuple):
-    intersection = [word for word in meta_tuple[0] if word in meta_tuple[1]]
-    return True if intersection else False
+def check_equal(pair):
+    return pair[0] == pair[1]
+
+if __name__ == '__main__':
+    with open('../data/homophones.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print(f'Word pair: {" & ".join(str(x) for x in row)}')
+            for word in row:
+                print(f'Pronunciation: {nysiis(word)}')
+            print("Match" if nysiis(row[0]) == nysiis(row[1]) else "No match")
+            print(fuzz.ratio(*row))
--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -5,6 +5,7 @@ from src.homophone import *
 
 class TestPreprocessing(unittest.TestCase):
 
-    def test_fuzzy_compare(self):
-        check = [check_equal(pair) for pair in get_all_pairs('../data/homophones.csv')]
-        self.assertTrue(all(i is True for i in check))
+    def test_homophones(self):
+        evaluation = [True if check_equal(pair) else False for pair in get_all_pairs('../data/homophones.csv')]
+        accuracy = (evaluation.count(True)) / len(evaluation)
+        print(f'\n{accuracy*100}% accuracy')
