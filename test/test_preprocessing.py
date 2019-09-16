17db668198290890ab251062e1e23291b2c3d70a
William Grigor <bjgrigor@gmail.com>
2019-09-16 13:39:10 -0500

    New unit test

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -11,6 +11,7 @@ class TestPreprocessing(unittest.TestCase):
 
     def setUp(self):
         self.training_set = make_training_set(pages=1)
+        self.sentences = load_sentences()
 
     def test_training_set_length(self):
         test = [True if len(item) == 2 else False for item in self.training_set]
@@ -31,3 +32,8 @@ class TestPreprocessing(unittest.TestCase):
         ])
         arr = add_padding(arr)
         self.assertEqual(len(arr[0]), len(arr[1]))
+
+    def test_no_duplicates(self):
+        sentence_set = {(i, j) for i, j in self.sentences}
+        print(len(self.sentences))
+        self.assertEqual(len(self.sentences), len(sentence_set))
