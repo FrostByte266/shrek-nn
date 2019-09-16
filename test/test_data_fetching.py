67a24b39a44152295a8a786afebe8437d54e3321
William Grigor <bjgrigor@gmail.com>
2019-09-16 11:29:26 -0500

    Reworked unit test

--- test/test_data_fetching.py
+++ test/test_data_fetching.py
@@ -9,6 +9,9 @@ from src.data_fetching import fetch_puns_list
 
 class TestExampleFetching(unittest.TestCase):
 
+    def setUp(self):
+        self.puns = fetch_puns_list(1)
+
     @mock.patch('requests.get')
     def test_data_fetching_not_found(self, mocked_get):
         type(mocked_get.return_value).status_code = mock.PropertyMock(return_value=404)
@@ -16,8 +19,7 @@ class TestExampleFetching(unittest.TestCase):
             fetch_puns_list(1)
 
     def test_data_fetching_ok(self):
-        self.assertNotEqual(len(fetch_puns_list(1)), 0)
+        self.assertNotEqual(len(self.puns), 0)
 
     def test_no_none_in_fetch(self):
-        puns = fetch_puns_list(1)
-        self.assertNotIn(None, puns)
+        self.assertNotIn(None, self.puns)
