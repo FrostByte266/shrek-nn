74988981e16e72b95cf1729d47ff67d175aca95f
William Grigor <bjgrigor@gmail.com>
2019-09-16 13:38:54 -0500

    Duplicate protection

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -23,14 +23,15 @@ def make_train_csv():
         lines = f.readlines()
     reg = re.compile(r'\n')
     blank = ''
-    data = [f'"{re.sub(reg, blank, line)}", 0' for line in lines]
+    data = list(set([f'"{re.sub(reg, blank, line)}", 0' for line in lines]))
     with open('data/sentences.csv', 'w+') as f:
         f.write("\n".join(data))
 
 def load_sentences():
     with open('/data/sentences.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
-        return([[str(row[0]), int(row[1])] for row in reader])
+        data = [[str(row[0]), int(row[1])] for row in reader]
+        return data
 
 def make_training_set(pages = 1, rand_seed=None):
     sentences = load_sentences()
@@ -39,6 +40,7 @@ def make_training_set(pages = 1, rand_seed=None):
         seed(rand_seed)
     training_set = sentences + puns
     shuffle(training_set)
+    print(f"Sentences: {len(sentences)}, Puns: {len(puns)}, Final: {len(training_set)}")
     return np.array(training_set, object)
 
 
